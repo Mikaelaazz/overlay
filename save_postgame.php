e93ce592bd29233cbdecef95f2f5222a6e038c2a
Mikaela <johaneswijaya341@gmail.com>
2025-06-21 20:57:59 +0700

    Update save_postgame.php

--- save_postgame.php
+++ save_postgame.php
@@ -7,7 +7,7 @@ header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Content-Type: application/json");
 
-// Handle preflight requests for CORS
+// Handle preflight requests for CORS 
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     http_response_code(200);
     exit();
